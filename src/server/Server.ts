be0472f4c55ed226f38eeffc42b2ca07402b3afa
Anderson Damasceno de Paula <andersondamascenodev@gmail>
2024-12-15 16:29:24 -0300

    feat: add folders structure

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import { router } from './routes';
+import 'dotenv/config';
 
 const server = express();
 
-server.get('/', (_req, res) => {
-    
-    return res.send('Olá dev');
-});
+server.use(express.json());
+server.use(router);
+
+
 
 export { server }
