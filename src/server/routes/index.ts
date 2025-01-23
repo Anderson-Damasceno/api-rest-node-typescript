7989987fb78c51aa62a876f69f5050d6b9161d39
Anderson Damasceno de Paula <andersondamascenodev@gmail>
2025-01-23 19:48:19 -0300

    basic structure of controllers methods

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Router } from "express";
+import { CidadesController } from "./../controllers";
 
 const router = Router();
 
-
+router.post('/cidades',CidadesController.createValidadtion, CidadesController.create as any);
+router.get('/cidades',CidadesController.getAllValidadtion, CidadesController.getAll as any);
+router.get('/cidades/:id',CidadesController.getByIdValidation, CidadesController.getById as any);
+router.put('/cidades/:id',CidadesController.updateByIdValidation, CidadesController.updateById as any);
+router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById as any);
 
 export {router};
